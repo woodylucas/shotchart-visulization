ce367a135170bad3e89f4c1341c2763fab46566e
woodylucas <github email address>
2019-04-08 09:31:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as d3 from  "d3"
+import d3Tip from "d3-tip"
+import d3Hexbin from "d3-hexbin"
+import shotchart from "d3-shotchart"
 
 class App extends Component {
+  state = {
+    players: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/players')
+    .then(resp => resp.json())
+    .then(players => this.setState( { players } ))
+  }
+
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id='shot-chart'>
+        </div>
+
       </div>
     );
   }
